b0eb0a36e1e9b30b6ab1e077259df8696716f031
an.nguyen <an.nguyen@9thwonder.com>
2022-04-25 15:51:16 +0700

    build: commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ignores: [(commit) => commit.includes('init')],
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'revert',
+        'wip'
+      ]
+    ]
+  }
+}
